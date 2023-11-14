4a9f55d60dde40c44e008a620f3234c548308801
maropiyo <maropiyooo@gmail.com>
2023-11-14 16:49:56 +0900

    タイトル画面のSceneManagerを作成

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// タイトル画面のSceneManager
+/// </summary>
+public class TitleSceneManager : MonoBehaviour
+{
+    // スタートボタンが押された時、ゲーム画面に遷移する
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
