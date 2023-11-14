dee4bcdf252f586edc30a34f800b92d6ff2b57ad
maropiyo <maropiyooo@gmail.com>
2023-11-14 22:45:20 +0900

    オブジェクトの生成処理を追加

---
+++ Assets/Project/GameScene/Scripts/ObjectGenerator.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ObjectGenerator : MonoBehaviour
+{
+    // オブジェクトリスト
+    public GameObject[] objectList;
+    // 現在のオブジェクト
+    private GameObject currentObject;
+    // 生成位置
+    public Vector2 spawnPosition = new Vector2(0, 2);
+
+    void Start()
+    {
+        GenerateRandomObject();
+    }
+
+    /// <summary>
+    /// オブジェクトをランダムに生成する
+    /// </summary>
+    void GenerateRandomObject()
+    {
+        // ランダムでオブジェクトを選択して生成
+        int randomIndex = Random.Range(0, objectList.Length);
+        currentObject = Instantiate(objectList[randomIndex], spawnPosition, Quaternion.identity);
+        currentObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    /// <summary>
+    /// オブジェクトを落下させる
+    /// </summary>
+    public void DropObject()
+    {
+        if (currentObject != null)
+        {
+            // 重力を有効化
+            currentObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+            // 現在のオブジェクトをリセット
+            currentObject = null;
+            // 次のオブジェクトを0.1秒後に生成
+            Invoke("GenerateRandomObject", 0.1f);
+        }
+    }
+}
