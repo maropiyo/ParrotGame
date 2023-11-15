8fd0bdf9452adea2aedcbc1ee417d95e3db4a1a8
maropiyo <maropiyooo@gmail.com>
2023-11-16 00:07:04 +0900

    不要な処理を削除

--- Assets/Project/GameScene/Scripts/ObjectEvolution.cs
+++ Assets/Project/GameScene/Scripts/ObjectEvolution.cs
@@ -6,17 +6,11 @@ public class ObjectEvolution : MonoBehaviour
     public string nextObjectTag;
     // 進化後のオブジェクトのPrefab
     public GameObject nextObjectPrefab;
-    // 現在の操作中のオブジェクトか
-    public bool isCurrentObject = false;
     // すでに進化しているか
     private bool hasEvolved = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // 現在操作中のオブジェクトの場合は何もしない
-        if (isCurrentObject) {
-            return;
-        }
         // 衝突したオブジェクトが同じタグかつ進化先が指定されている場合
         if (collision.gameObject.CompareTag(tag) && nextObjectPrefab != null)
         {
