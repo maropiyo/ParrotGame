755d5abd3a1aa916e5fbaf45fb7d2ee6346d6107
maropiyo <maropiyooo@gmail.com>
2023-11-18 17:29:46 +0900

    ok

--- Assets/Project/GameScene/Scripts/ObjectEvolution.cs
+++ Assets/Project/GameScene/Scripts/ObjectEvolution.cs
@@ -7,24 +7,27 @@ public class ObjectEvolution : MonoBehaviour
     // 進化後のオブジェクトのPrefab
     public GameObject nextObjectPrefab;
     // すでに進化しているか
-    private bool hasEvolved = false;
+    private bool isEvolved = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // 衝突したオブジェクトが同じタグかつ進化先が指定されている場合
-        if (collision.gameObject.CompareTag(tag) && nextObjectPrefab != null)
+        // 衝突したオブジェクトが同じタグ場合
+        if (collision.gameObject.CompareTag(tag))
         {
             // 衝突したオブジェクトを破棄
             Destroy(collision.gameObject);
 
-            if (!hasEvolved)
+            // ダチョウ同士の場合は何もしない
+            if (collision.gameObject.CompareTag("Ostrich")) return;
+
+            if (isEvolved == false && collision.gameObject.GetComponent<ObjectEvolution>().isEvolved == false)
             {
+                isEvolved = true;
+
                 // 進化後のオブジェクトを生成
                 Instantiate(nextObjectPrefab, transform.position, Quaternion.identity);
-
-                // 接触したオブジェクトに対してすでに進化していることを伝える
-                collision.gameObject.GetComponent<ObjectEvolution>().hasEvolved = true;
             }
         }
+
     }
 }
