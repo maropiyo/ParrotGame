355ef695ef7aeb9c421a595c18d839b4d7cd7b28
maropiyo <maropiyooo@gmail.com>
2023-11-23 23:37:09 +0900

    ok

--- Assets/Project/GameScene/Scripts/CurrentObjectMover.cs
+++ Assets/Project/GameScene/Scripts/CurrentObjectMover.cs
@@ -4,16 +4,22 @@ public class CurrentObjectMover : MonoBehaviour
 {
     // 現在のオブジェクト
     public GameObject currentObject = null;
+
     // 現在のオブジェクトの半分大きさ
-    private float halfScaleX;
+    private float halfObjectRadius;
+
     // ドラッグ開始位置
     private Vector3 touchStartPos;
+
     // オブジェクトの初期位置
     private Vector3 objectStartPos;
+
     // 左制限
     private float leftLimit = -2.69f;
+
     // 右制限
     private float rightLimit = 2.69f;
+
     // ドラッグ中か
     private bool isDragging = false;
 
@@ -21,35 +27,47 @@ public class CurrentObjectMover : MonoBehaviour
     {
         if (currentObject)
         {
-            // オブジェクトのスケールとコライダーの半径を掛け合わせることでオブジェクトの半径を計算
-            halfScaleX = currentObject.GetComponent<Transform>().localScale.x * currentObject.GetComponent<CircleCollider2D>().radius;
-            // ドラッグ開始
+            // オブジェクトの半径を計算
+            CalculateObjectRadius();
+
             if (Input.GetMouseButtonDown(0))
             {
+                // ドラッグ開始
                 OnDragStart();
             }
-            // ドラッグ中
             else if (isDragging && Input.GetMouseButton(0))
             {
+                // ドラッグ中
                 OnDragging();
             }
-            // ドラッグ終了
             else if (Input.GetMouseButtonUp(0))
             {
+                // ドラッグ終了
                 OnDragEnd();
             }
         }
     }
 
+    private void CalculateObjectRadius()
+    {
+        // オブジェクトのスケールとコライダーの半径を掛け合わせることでオブジェクトの半径を計算
+        CircleCollider2D collider = currentObject.GetComponent<CircleCollider2D>();
+        halfObjectRadius = currentObject.transform.localScale.x * collider.radius;
+    }
+
     private void OnDragStart()
     {
+        // ドラッグ開始位置を記録
         touchStartPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // オブジェクトの初期位置を記録
         objectStartPos = currentObject.transform.position;
+        // ドラッグ中フラグを有効にする
         isDragging = true;
     }
 
     private void OnDragEnd()
     {
+        // ドラッグ中フラグを無効にする
         isDragging = false;
     }
 
@@ -59,10 +77,9 @@ public class CurrentObjectMover : MonoBehaviour
         Vector3 offset = Camera.main.ScreenToWorldPoint(Input.mousePosition) - touchStartPos;
 
         // 新しいX座標を計算し、制限内でクランプする
-        float newXPos = Mathf.Clamp(objectStartPos.x + offset.x, leftLimit + halfScaleX, rightLimit - halfScaleX);
+        float newXPos = Mathf.Clamp(objectStartPos.x + offset.x, leftLimit + halfObjectRadius, rightLimit - halfObjectRadius);
 
         // オブジェクトの位置を更新する
         currentObject.transform.position = new Vector3(newXPos, objectStartPos.y, objectStartPos.z);
     }
 }
-
