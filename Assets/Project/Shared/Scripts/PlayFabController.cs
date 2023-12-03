b7f7e881a2b75de414110a956b06014d06b5f792
maropiyo <maropiyooo@gmail.com>
2023-12-03 14:28:31 +0900

    Add singleton pattern and score submission and leaderboard retrieval methods

--- Assets/Project/Shared/Scripts/PlayFabController.cs
+++ Assets/Project/Shared/Scripts/PlayFabController.cs
@@ -1,9 +1,31 @@
 using UnityEngine;
+using System.Collections.Generic;
 using PlayFab.ClientModels;
 using PlayFab;
 
 public class PlayFabController : MonoBehaviour
 {
+    public static PlayFabController Instance;
+    public bool DontDestroyEnabled = true;
+
+    void Awake()
+    {
+        // シングルトンパターンの実装
+        if (Instance == null)
+        {
+            Instance = this;
+
+            if (DontDestroyEnabled)
+            {
+                // Sceneを遷移してもオブジェクトが消えないようにする
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     private void OnEnable()
     {
         PlayFabAuthService.OnLoginSuccess += PlayFabAuthService_OnLoginSuccess;
@@ -28,5 +50,55 @@ public class PlayFabController : MonoBehaviour
     void Start()
     {
         PlayFabAuthService.Instance.Authenticate(Authtypes.Silent);
+    }
+
+    /// <summary>
+    /// スコアを送信する。
+    /// </summary>
+    /// <param name="playerScore"></param>
+    public void SubmitScore(int playerScore)
+    {
+        PlayFabClientAPI.UpdatePlayerStatistics(new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName = "HighScoreRanking",
+                    Value = playerScore
+                }
+            }
+        }, result =>
+        {
+            Debug.Log($"スコア {playerScore} 送信完了！");
+        }, error =>
+        {
+            Debug.Log(error.GenerateErrorReport());
+        });
+    }
+
+    /// <summary>
+    /// ランキングを取得する。
+    /// </summary>
+    public void GetLeaderboard()
+    {
+        PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest
+        {
+            StatisticName = "HighScoreRanking"
+        }, result =>
+        {
+            foreach (var item in result.Leaderboard)
+            {
+                string displayName = item.DisplayName;
+                if (displayName == null)
+                {
+                    displayName = "NoName";
+                }
+                Debug.Log($"{item.Position + 1}位:{displayName} " + $"スコア {item.StatValue}");
+            }
+        }, error =>
+        {
+            Debug.Log(error.GenerateErrorReport());
+        });
     }
 }
