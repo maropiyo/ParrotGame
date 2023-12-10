9f8b5ce03ab0e53ca57db1eff26d1367ba89ec1a
maropiyo <maropiyooo@gmail.com>
2023-12-10 12:18:08 +0900

    Fix object position checker and game scene manager

--- Assets/Project/GameScene/Scripts/GameSceneManager.cs
+++ Assets/Project/GameScene/Scripts/GameSceneManager.cs
@@ -6,8 +6,26 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class GameSceneManager : MonoBehaviour
 {
+    // メニューポップアップ
+    public GameObject menuPopup;
+    // リザルトポップアップ
+    public GameObject resultPopup;
+    // ScoreManagerコンポーネント
+    private ScoreManager scoreManager;
+    // SoundEffectPlayerコンポーネント
+    private SEPlayer soundEffectPlayer;
+    // GameSceneManagerコンポーネント
+    private GameSceneManager gameSceneManager;
+
+
     void Start()
     {
+        // ScoreManagerオブジェクトのScoreManagerコンポーネントを取得
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+
+        // ゲームを再開する。
+        Resume();
+
         // インタースティシャル広告をロードする。
         // GoogleMobileAdsManager.Instance.LoadInterstitialAd();
     }
@@ -15,6 +33,74 @@ public class GameSceneManager : MonoBehaviour
     // タイトル画面をロードする
     public void LoadTitleScene()
     {
+        // タイトル画面をロードする。
         SceneManager.LoadScene("TitleScene");
     }
+
+    // ゲームオーバー時の処理
+    public void GameOver()
+    {
+        // プレイヤーの動きを止める
+        GameObject.Find("Player").GetComponent<PlayerMover>().canMove = false;
+        // ベストスコアを保存
+        scoreManager.SaveBestScore();
+
+        // リザルトポップアップを表示する。
+        ShowResultPopup();
+    }
+
+    // メニューポップアップを表示する
+    public void ShowMenuPopup()
+    {
+        // ゲームを一時停止する。
+        Pause();
+
+        // メニューポップアップを表示する。
+        menuPopup.SetActive(true);
+    }
+
+    // メニューポップアップを閉じる
+    public void CloseMenuPopup()
+    {
+        // ゲームを再開する。
+        Resume();
+
+        // メニューポップアップを非表示にする。
+        menuPopup.SetActive(false);
+    }
+
+    // リザルトポップアップを表示する
+    public void ShowResultPopup()
+    {
+        // ゲームを一時停止する。
+        Pause();
+
+        // リザルトポップアップを表示する。
+        resultPopup.SetActive(true);
+    }
+
+    // リザルトポップアップを閉じる
+    public void CloseResultPopup()
+    {
+        // ゲームを再開する。
+        Resume();
+
+        // リザルトポップアップを非表示にする。
+        resultPopup.SetActive(false);
+    }
+
+    // ポーズする
+    public void Pause()
+    {
+        // ゲームを一時停止する。
+        Time.timeScale = 0;
+    }
+
+    // ポーズを解除する
+    public void Resume()
+    {
+        // ゲームを再開する。
+        Time.timeScale = 1;
+    }
+
 }
--- Assets/Project/GameScene/Scripts/ObjectPositionChecker.cs
+++ Assets/Project/GameScene/Scripts/ObjectPositionChecker.cs
@@ -12,11 +12,10 @@ public class ObjectPositionChecker : MonoBehaviour
 
     void Start()
     {
-        enabled = true;
-        // ScoreManagerオブジェクトのScoreManagerコンポーネントを取得
-        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
         // GameSceneManagerオブジェクトのGameSceneManagerコンポーネントを取得
         gameSceneManager = GameObject.Find("GameSceneManager").GetComponent<GameSceneManager>();
+        // チェックを有効にする
+        enabled = true;
     }
 
     private void FixedUpdate()
@@ -31,39 +30,11 @@ public class ObjectPositionChecker : MonoBehaviour
         // 画面外に出たかどうかを判定
         if (enabled && (screenPosition.x < 0 || screenPosition.x > Screen.width || screenPosition.y < 0 || screenPosition.y > Screen.height))
         {
-            // 一回だけ処理を行う
+            // チェックを無効にする
             enabled = false;
-            // プレイヤーの動きを止める
-            GameObject.Find("Player").GetComponent<PlayerMover>().canMove = false;
-            // ベストスコアを保存
-            scoreManager.SaveBestScore();
-            StartCoroutine(PauseAndLoadCoroutine());
+
+            // ゲームオーバー処理
+            gameSceneManager.GameOver();
         }
-        else
-        {
-            // 画面内にある場合は処理を継続
-            enabled = true;
-        }
-    }
-
-
-    IEnumerator PauseAndLoadCoroutine()
-    {
-        // 動きを停止
-        Time.timeScale = 0;
-
-        // 一定時間待つ
-        yield return new WaitForSecondsRealtime(2.0f);
-
-        // インタースティシャル広告を表示する
-        // GoogleMobileAdsManager.Instance.ShowInterstitialAd();
-
-        // ゲームの時間を元に戻す
-        Time.timeScale = 1f;
-
-        enabled = true;
-
-        // タイトルシーンに切り替える
-        gameSceneManager.LoadTitleScene();
     }
 }
